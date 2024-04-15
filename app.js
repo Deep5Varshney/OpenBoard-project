9caa6d16647e942700e4ff4466e2866ca8fbd078
Deep5Varshney <137312194+Deep5Varshney@users.noreply.github.com>
2024-04-16 00:21:04 +0530

    update

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const cors = require("cors"); // Import cors
+const http = require("http"); // Import http module
 const socket = require("socket.io");
 
 const app = express();
+app.use(cors({ origin: 'http://192.168.1.104:5500' }));
 app.use(express.static("public"));
-let port = 5000;
-let server = app.listen(port, ()=>{
+let port = process.env.PORT || 5000;
+let server = http.createServer(app); // Create an HTTP server
+ server.listen(port, ()=>{
     console.log("listening on port" + port);
 })
 
-let io = socket(server);
+let io = socket(server)(server, {
+    cors: {
+        origin: "*",
+    }
+});
 io.on("connection", (socket)=>{
     console.log("made socket connection");
 
